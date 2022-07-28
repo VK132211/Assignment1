77badc34d81a24da4a5ed3148ed993cae9026349
VK132211 <m.vk.80899@gmail.com>
2022-07-28 10:18:30 +0530

    small modification in Q2

--- Assignment1/src/assignment/Q2.java
+++ Assignment1/src/assignment/Q2.java
@@ -2,15 +2,22 @@ package assignment;
 
 public class Q2 {
 	public static void main(String args[]) {
-//		1 1 1 1 
-//		2 2 2 2 
-//		3 3 3 3 
-//		4 4 4 4 
+//		1 1 1 1
+//		2 2 2 2
+//		3 3 3 3
+//		4 4 4 4
+
 
 		int n = 4;
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
+				if(j!=n) {
 				System.out.print(i + " ");
+				}
+				else
+				{
+					System.out.print(i);
+				}
 			}
 			System.out.println();
 		}
